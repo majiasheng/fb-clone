67dd0dca170934f9185ef303ec4d529640c98ef3
majiasheng <jiasheng.ma@yahoo.com>
2017-05-29 19:15:10 -0400

    minor bug fix

--- src/registration.php
+++ src/registration.php
@@ -4,8 +4,6 @@ session_start();
 <p>Sign Me Up</p>
 <?php
 
-// require_once("db_connect.php");
-require("user.php");
 require("../include/functions.php");
 
 // define error message variables to indicate errors
