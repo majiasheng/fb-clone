9a05ce813af19e357912eda97e26c348383f0953
melanie0926 <melanieeelin@gmail.com>
2017-05-29 21:06:34 -0400

    added post/comment tables

--- src/user.sql
+++ src/user.sql
@@ -12,4 +12,26 @@ CREATE TABLE users (
 	PRIMARY KEY(id)
 );
 
+CREATE TABLE posts (
+	post_id		INTEGER		NOT NULL AUTO_INCREMENT,
+	user_id		INTEGER		NOT NULL,
+	content		TEXT		NOT NULL,
+	post_date	date 		NOT NULL,
+	comment_id	INTEGER,
+
+	PRIMARY KEY(post_id),
+	FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE comments (
+	comment_id  INTEGER 	NOT NULL AUTO_INCREMENT,
+	author		VARCHAR(30) NOT NULL,
+	comment_date date 		NOT NULL,
+
+
+	PRIMARY KEY(comment_id),
+	FOREIGN KEY (comment_id) REFERENCES posts(comment_id)
+);
+
+
 INSERT INTO users (first_name, last_name, email, password, birth_month, birth_day, birth_year, gender) VALUES ('Jia-Sheng', 'Ma', 'majiasheng@fb-lets-go.com', 'password', 'Jan', '01', '1900', 'M');
