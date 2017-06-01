1100e1e9ffeba8b1db282368bacb301198f3f58e
melanie0926 <melanieeelin@gmail.com>
2017-06-01 04:51:03 -0400

    created update info page

--- include/scripts/js/script.js
+++ include/scripts/js/script.js
@@ -1,4 +1,4 @@
-// show the dropdown menu
+/* Drop-down Menu */
 function show_setting_menu() {
 	var dropdown = document.getElementsByClassName("icon-setting--dropdown")[0];
 	 if(dropdown.style.display == "block") { 
@@ -18,4 +18,18 @@ window.onclick = function(event) {
 	  	}
 
   }
+}
+
+/* Update Info Page */
+var modal = document.getElementById('update-info-page');				// get the page
+var btn = document.getElementById("update-info-btn");					// button to open the page
+var span = document.getElementsByClassName("modal__page-close")[0];		// the close button
+
+// open update-info page onclide
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+// close onclick
+span.onclick = function() {
+    modal.style.display = "none";
 }
