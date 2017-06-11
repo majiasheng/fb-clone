13d7859de89373bc7bc90a8fc3aa29b5c9ef1b4d
melanie0926 <melanieeelin@gmail.com>
2017-06-10 22:20:30 -0400

    able to add profile photo; modified css

--- include/loads/load_images.php
+++ include/loads/load_images.php
@@ -3,12 +3,19 @@
 // load cover pic
 function load_cover($user) {
     // if folder exists, meaning that the user has updated cover, update, else use default
-    if(file_exists(PATH_SRC_TO_RSRC_IMG.$user->get_email().'/cover'))
-        return PATH_SRC_TO_RSRC_IMG.$user->get_email().'/cover/cover_img';
+    if(file_exists(PATH_TO_USERS.$user->get_email().'/cover'))
+        return PATH_TO_USERS.$user->get_email().'/cover/cover_img';
     else
-        return IMG_COVER;
+        return DEFAULT_COVER;
 }
 
+// load profile photo
+function load_profile($user) {
+	if(file_exists(PATH_TO_USERS.$user->get_email().'/profile'))
+        return PATH_TO_USERS.$user->get_email().'/profile/profile_img';
+    else
+        return DEFAULT_PROFILE;
+}
 
 
 ?>
