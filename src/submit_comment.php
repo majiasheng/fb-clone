d48bf052509c42d593059d8a547bece30fce4487
Unknown <bin.zhou.1@stonybrook.edu>
2017-06-11 01:17:41 -0400

    restore the comment button to previous style

--- src/submit_comment.php
+++ src/submit_comment.php
@@ -1,5 +1,7 @@
 <?php
 require_once("../include/functions.php");
+require_once("../include/loads/load_images.php");
+
 session_start();
 
 $user = $_SESSION['user'];
@@ -25,11 +27,43 @@ if(isset($_POST) && isset($_POST['post_comment_content'])) {
 
 if(!is_null($comments)):
 
+/*
+
+    <?php
+    foreach ($comments as $c) {
+        $pic = load_profile_email($c->getAuthor());
+
+        echo '
+            <div class="comment__details">  
+                <img src="'. $pic .'" class="comment__pic">
+                <div class="commenter">
+                    <p class="comment__header__info info__commentor"><a href="#">' . getUserNameByEmail($c->getAuthor(), $pdo) . '</a></p>
+                    <p class="comment__content__p">'. $c->getCommentContent() .'</p>
+
+                    <p class="comment__header__info info__comment__date">'. $c->getCommentTime() .'</p>
+                </div>
+
+            </div>
+        ';
+    }
+    ?>
+*/
+
+
 foreach ($comments as $c):
+$pic = load_profile_email($c->getAuthor());
 
 ?>
 
-<div class="comment__content__p"><?php echo $c->getCommentContent() . ' (' . getUserNameByEmail($c->getAuthor(), $pdo) ?>)</div>
+<div class="comment__details">  
+    <img src="<?php echo $pic ?>" class="comment__pic">
+    <div class="commenter">
+        <p class="comment__header__info info__commentor"><a href="#"><?php echo getUserNameByEmail($c->getAuthor(), $pdo) ?></a></p>
+        <p class="comment__content__p"><?php echo $c->getCommentContent() ?></p>
+
+        <p class="comment__header__info info__comment__date"><?php echo $c->getCommentTime() ?></p>
+    </div>
+</div>
 
 <?php endforeach ?>
 
